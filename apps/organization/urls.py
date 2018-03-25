1693a54c6f762285aa3671e982a0a55f2a042d93
ksino <flycat86@163.com>
2018-03-25 17:01:27 +0800

    delete a url

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -4,7 +4,7 @@
 from django.conf.urls import url, include
 
 from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, AddFavView
-from .views import TeacherListView, TeacherDetailView, importrecord
+from .views import TeacherListView, TeacherDetailView
 
 
 urlpatterns = [
@@ -17,7 +17,6 @@ urlpatterns = [
     url(r'^desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name="org_desc"),
     url(r'^org_teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name="org_teacher"),
     url(r'^add_fav/$', AddFavView.as_view(), name="add_fav"),
-    url(r'^import/$', importrecord.as_view(), name="importrecord"),
     # teacher list
     url(r'^teacher/list/$', TeacherListView.as_view(), name="teacher_list"),
     url(r'^teacher/detail/(?P<teacher_id>\d+)/$', TeacherDetailView.as_view(), name="teacher_detail"),
